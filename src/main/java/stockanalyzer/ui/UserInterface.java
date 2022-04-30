db35518294391214e0f00ebfbbca3de31487adc0
Christoph <christoph.thonhauser@squer.at>
2022-04-30 18:01:27 +0200

    enable custom user input

--- src/main/java/stockanalyzer/ui/UserInterface.java
+++ src/main/java/stockanalyzer/ui/UserInterface.java
@@ -7,80 +7,78 @@ import java.io.InputStreamReader;
 
 import stockanalyzer.ctrl.Controller;
 
-public class UserInterface 
-{
+public class UserInterface {
 
-	private Controller ctrl = new Controller();
+    private Controller ctrl = new Controller();
 
-	public void getDataFromCtrl1() {
-		ctrl.process("AAPL,AMZN,BABA");
-	}
+    public void getDataFromCtrl1() {
+        ctrl.process("AAPL,AMZN,BABA");
+    }
 
-	public void getDataFromCtrl2(){
-		ctrl.process("AMZN");
-	}
+    public void getDataFromCtrl2() {
+        ctrl.process("AMZN");
+    }
 
-	public void getDataFromCtrl3(){
-		ctrl.process("BABA");
-	}
-	public void getDataFromCtrl4(){
-		ctrl.process("loremipsum");
-	}
-	
-	public void getDataForCustomInput() {
-		
-	}
+    public void getDataFromCtrl3() {
+        ctrl.process("BABA");
+    }
 
+    public void getDataFromCtrl4() {
+        ctrl.process("loremipsum");
+    }
 
-	public void start() {
-		Menu<Runnable> menu = new Menu<>("User Interface");
-		menu.setTitel("Wählen Sie aus:");
-		menu.insert("a", "Apple Amazon Alibaba", this::getDataFromCtrl1);
-		menu.insert("b", "Amazon", this::getDataFromCtrl2);
-		menu.insert("c", "Alibaba", this::getDataFromCtrl3);
-		menu.insert("d", "Choice User Input:",this::getDataForCustomInput);
-		menu.insert("z", "Choice incorrect Ticket:",this::getDataFromCtrl4);
-		menu.insert("q", "Quit", null);
-		Runnable choice;
-		while ((choice = menu.exec()) != null) {
-			 choice.run();
-		}
-		ctrl.closeConnection();
-		System.out.println("Program finished");
-	}
+    public void getDataForCustomInput() {
+        ctrl.process(readLine());
+    }
 
 
-	protected String readLine() 
-	{
-		String value = "\0";
-		BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			value = inReader.readLine();
-		} catch (IOException e) {
-		}
-		return value.trim();
-	}
+    public void start() {
+        Menu<Runnable> menu = new Menu<>("User Interface");
+        menu.setTitel("Wählen Sie aus:");
+        menu.insert("a", "Apple Amazon Alibaba", this::getDataFromCtrl1);
+        menu.insert("b", "Amazon", this::getDataFromCtrl2);
+        menu.insert("c", "Alibaba", this::getDataFromCtrl3);
+        menu.insert("d", "Choice User Input:", this::getDataForCustomInput);
+        menu.insert("z", "Choice incorrect Ticket:", this::getDataFromCtrl4);
+        menu.insert("q", "Quit", null);
+        Runnable choice;
+        while ((choice = menu.exec()) != null) {
+            choice.run();
+        }
+        ctrl.closeConnection();
+        System.out.println("Program finished");
+    }
 
-	protected Double readDouble(int lowerlimit, int upperlimit) 
-	{
-		Double number = null;
-		while(number == null) {
-			String str = this.readLine();
-			try {
-				number = Double.parseDouble(str);
-			}catch(NumberFormatException e) {
-				number=null;
-				System.out.println("Please enter a valid number:");
-				continue;
-			}
-			if(number<lowerlimit) {
-				System.out.println("Please enter a higher number:");
-				number=null;
-			}else if(number>upperlimit) {
-				System.out.println("Please enter a lower number:");
-				number=null;
-			}
-		}
-		return number;
-	}
+
+    protected String readLine() {
+        String value = "\0";
+        BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            value = inReader.readLine();
+        } catch (IOException e) {
+        }
+        return value.trim();
+    }
+
+    protected Double readDouble(int lowerlimit, int upperlimit) {
+        Double number = null;
+        while (number == null) {
+            String str = this.readLine();
+            try {
+                number = Double.parseDouble(str);
+            } catch (NumberFormatException e) {
+                number = null;
+                System.out.println("Please enter a valid number:");
+                continue;
+            }
+            if (number < lowerlimit) {
+                System.out.println("Please enter a higher number:");
+                number = null;
+            } else if (number > upperlimit) {
+                System.out.println("Please enter a lower number:");
+                number = null;
+            }
+        }
+        return number;
+    }
 }
